b429e4ccf548eb33f36a3b4cf3a2965d06aef489
carlykusa <shota.lovenattsu@gmail.com>
2025-04-04 19:01:56 +0900

    Dockerコンテナ作成完了

--- el_system/el_system/settings.py
+++ el_system/el_system/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'el_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
